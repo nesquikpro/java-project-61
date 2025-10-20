27310736c20d8fa6d6ffeb6b9a57689021bd4644
nesquikpro <nnsquik@outlook.com>
2025-10-20 15:38:29 +0300

    project setup

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,5 +1,6 @@
-class App {
+package  hexlet.code;
+public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Welcome to the Brain Games!");
     }
 }
