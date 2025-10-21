05398e3e5447c74d395ed5f1ace3ef32041e3e4c
nesquikpro <nnsquik@outlook.com>
2025-10-21 12:54:32 +0300

    fix project issues from sonarqube

--- src/main/java/hexlet/code/App.java
+++ src/main/java/hexlet/code/App.java
@@ -3,7 +3,6 @@ package  hexlet.code;
 public class App {
     public static void main(String[] args) {
         System.out.println("Welcome to the Brain Games!");
-        Cli cli = new Cli();
-        cli.getName();
+        Cli.getName();
     }
 }
--- src/main/java/hexlet/code/Cli.java
+++ src/main/java/hexlet/code/Cli.java
@@ -3,10 +3,16 @@ package hexlet.code;
 import java.util.Scanner;
 
 public class Cli{
+
+    private Cli(){
+
+    }
+
     public static void getName(){
         Scanner sc = new Scanner(System.in);
         System.out.print("May I have your name? ");
         String name = sc.nextLine();
         System.out.print("Hello, " + name + "!");
+        sc.close();
     }
 }
